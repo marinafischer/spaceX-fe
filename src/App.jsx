793e07bf040a380e81103fe029c8e10f43b36b8f
marinafischer <marinamirandafischer@gmail.com>
2022-10-21 17:29:15 -0300

    feat: init aplication

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <p>Teste</p>
+  );
+}
+
+export default Main;
